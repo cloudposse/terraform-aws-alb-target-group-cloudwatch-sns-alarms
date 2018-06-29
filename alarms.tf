31102c9f786e927622f1c41134c6ec1b02ba0ac2
Sarkis Varozian <svarozian@gmail.com>
2018-06-28 17:13:47 -0700

    initial commit

---
+++ alarms.tf
@@ -0,0 +1,109 @@
+module "default_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+}
+module "httpcode_alarm_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  attributes = "${compact(concat(var.attributes, list("%v", "count", "high")))}"
+}
+
+module "target_response_time_alarm_label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
+  name       = "${var.name}"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  attributes = "${compact(concat(var.attributes, list("target", "response", "high")))}"
+}
+
+locals {
+  thresholds = {
+    Target3XXCountThreshold     = "${max(var.target_3xx_count_threshold, 0)}"
+    Target4XXCountThreshold     = "${max(var.target_4xx_count_threshold, 0)}"
+    Target5XXCountThreshold     = "${max(var.target_5xx_count_threshold, 0)}"
+    TargetResponseTimeThreshold = "${max(var.target_response_time_threshold, 0)}"
+  }
+
+  dimensions_map = {
+    "targetgroup" = {
+      "TargetGroup"  = "${join("/", list("targetgroup", var.target_group_name, var.target_group_arn_suffix))}"
+      "LoadBalancer" = "${join("/", list("app", var.alb_name, var.alb_arn_suffix))}"
+    }
+
+    "loadbalancer" = {
+      "LoadBalancer" = "${join("/", list("app", var.alb_name, var.alb_arn_suffix))}"
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "httpcode_target_3xx_count" {
+  count               = "${local.enabled}"
+  alarm_name          = "${format(module.httpcode_alarm_label.id, "3XX")}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "${var.evaluation_periods}"
+  metric_name         = "HTTPCode_Target_3XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = "${var.period}"
+  statistic           = "Sum"
+  threshold           = "${local.thresholds["Target3XXCountThreshold"]}"
+  alarm_description   = "${format(var.httpcode_alarm_description, "3XX", module.default_label.id, local.thresholds["Target3XXCountThreshold"], var.period/60, var.evaluation_periods)}"
+  alarm_actions       = ["${aws_sns_topic.default.arn}"]
+  ok_actions          = ["${aws_sns_topic.default.arn}"]
+
+  dimensions = "${local.dimensions_map[var.target_group_name == "" ? "loadbalancer" : "targetgroup"]}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "httpcode_target_4xx_count" {
+  count               = "${local.enabled}"
+  alarm_name          = "${format(module.httpcode_alarm_label.id, "4XX")}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "${var.evaluation_periods}"
+  metric_name         = "HTTPCode_Target_4XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = "${var.period}"
+  statistic           = "Sum"
+  threshold           = "${local.thresholds["Target4XXCountThreshold"]}"
+  alarm_description   = "${format(var.httpcode_alarm_description, "4XX", module.default_label.id, local.thresholds["Target4XXCountThreshold"], var.period/60, var.evaluation_periods)}"
+  alarm_actions       = ["${aws_sns_topic.default.arn}"]
+  ok_actions          = ["${aws_sns_topic.default.arn}"]
+
+  dimensions = "${local.dimensions_map[var.target_group_name == "" ? "loadbalancer" : "targetgroup"]}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "httpcode_target_5xx_count" {
+  count               = "${local.enabled}"
+  alarm_name          = "${format(module.httpcode_alarm_label.id, "5XX")}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "${var.evaluation_periods}"
+  metric_name         = "HTTPCode_Target_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = "${var.period}"
+  statistic           = "Sum"
+  threshold           = "${local.thresholds["Target5XXCountThreshold"]}"
+  alarm_description   = "${format(var.httpcode_alarm_description, "5XX", module.default_label.id, local.thresholds["Target5XXCountThreshold"], var.period/60, var.evaluation_periods)}"
+  alarm_actions       = ["${aws_sns_topic.default.arn}"]
+  ok_actions          = ["${aws_sns_topic.default.arn}"]
+
+  dimensions = "${local.dimensions_map[var.target_group_name == "" ? "loadbalancer" : "targetgroup"]}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "target_response_time" {
+  count               = "${local.enabled}"
+  alarm_name          = "${format(module.target_response_time_alarm_label.id)}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "${var.evaluation_periods}"
+  metric_name         = "TargetResponseTime"
+  namespace           = "AWS/ApplicationELB"
+  period              = "${var.period}"
+  statistic           = "Average"
+  threshold           = "${local.thresholds["TargetResponseTimeThreshold"]}"
+  alarm_description   = "${format(var.target_response_time_alarm_description, module.default_label.id, local.thresholds["TargetResponseTimeThreshold"], var.period/60, var.evaluation_periods)}"
+  alarm_actions       = ["${aws_sns_topic.default.arn}"]
+  ok_actions          = ["${aws_sns_topic.default.arn}"]
+
+  dimensions = "${local.dimensions_map[var.target_group_name == "" ? "loadbalancer" : "targetgroup"]}"
+}
---
+++ variables.tf
@@ -0,0 +1,109 @@
+variable "name" {
+  type        = "string"
+  description = "Name (unique identifier for app or service)"
+}
+
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "delimiter" {
+  type        = "string"
+  description = "The delimiter to be used in labels."
+  default     = "-"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "attributes" {
+  type        = "list"
+  description = "List of attributes to add to label."
+  default     = []
+}
+
+variable "tags" {
+  type        = "map"
+  description = "Map of key-value pairs to use for tags."
+  default     = {}
+}
+
+variable "enabled" {
+  type        = "string"
+  description = "Whether to create all resources"
+  default     = "true"
+}
+
+variable "alb_name" {
+  type        = "string"
+  description = "The name of ALB to monitor."
+}
+
+variable "alb_arn_suffix" {
+  type        = "string"
+  description = "The ARN suffix of ALB."
+}
+
+variable "target_group_name" {
+  type        = "string"
+  description = "The name of the ALB Target Group to monitor."
+  default     = ""
+}
+
+variable "target_group_arn_suffix" {
+  type        = "string"
+  description = "The ARN suffix of ALB Target Group."
+  default     = ""
+}
+
+
+variable "evaluation_periods" {
+  type        = "string"
+  description = "Number of periods to evaluate for the alarm."
+  default     = "1"
+}
+
+variable "period" {
+  type        = "string"
+  description = "Duration in seconds to evaluate for the alarm."
+  default     = "300"
+}
+
+variable "target_3xx_count_threshold" {
+  type        = "string"
+  description = "The maximum count of 3XX requests over a period."
+  default     = "25"
+}
+
+variable "target_4xx_count_threshold" {
+  type        = "string"
+  description = "The maximum count of 4XX requests over a period."
+  default     = "25"
+}
+
+variable "target_5xx_count_threshold" {
+  type        = "string"
+  description = "The maximum count of 5XX requests over a period."
+  default     = "25"
+}
+
+variable "httpcode_alarm_description" {
+  type        = "string"
+  description = "The string to format and use as the httpcode alarm description."
+  default     = "HTTPCode %v count for %v over %v last %d minute(s) over %v period(s)"
+}
+
+variable "target_response_time_threshold" {
+  type = "string"
+  description = "The maximum average target response time (in seconds) over a period."
+  default = "0.5"
+}
+
+variable "target_response_time_alarm_description" {
+  type        = "string"
+  description = "The string to format and use as the target response time alarm description."
+  default     = "Target Response Time average for %v over %v last %d minute(s) over %v period(s)"
+}
