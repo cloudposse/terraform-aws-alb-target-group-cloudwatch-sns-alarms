159a0786264066f20dae1edc1f91eb8cb272b46b
Marc Tamsky <tamsky@users.noreply.github.com>
2018-07-20 17:51:46 -0700

    expose treat_missing_data, and add new {ok,alarm,insufficient_data}_actions (#10)

--- alarms.tf
+++ alarms.tf
@@ -1,4 +1,9 @@
 locals {
+  # default to using notify_arns unless a more specific action is specified.
+  alarm_actions             = "${coalescelist(var.alarm_actions,var.notify_arns)}"
+  ok_actions                = "${coalescelist(var.ok_actions,var.notify_arns)}"
+  insufficient_data_actions = "${coalescelist(var.insufficient_data_actions,var.notify_arns)}"
+
   thresholds = {
     target_3xx_count     = "${max(var.target_3xx_count_threshold, 0)}"
     target_4xx_count     = "${max(var.target_4xx_count_threshold, 0)}"
@@ -26,52 +31,58 @@ module "httpcode_alarm_label" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "httpcode_target_3xx_count" {
-  count               = "${local.target_3xx_alarm_enabled}"
-  alarm_name          = "${format(module.httpcode_alarm_label.id, "3XX")}"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "${var.evaluation_periods}"
-  metric_name         = "HTTPCode_Target_3XX_Count"
-  namespace           = "AWS/ApplicationELB"
-  period              = "${var.period}"
-  statistic           = "Sum"
-  threshold           = "${local.thresholds["target_3xx_count"]}"
-  alarm_description   = "${format(var.httpcode_alarm_description, "3XX", module.default_label.id, local.thresholds["target_3xx_count"], var.period/60, var.evaluation_periods)}"
-  alarm_actions       = ["${var.notify_arns}"]
-  ok_actions          = ["${var.notify_arns}"]
+  count                     = "${local.target_3xx_alarm_enabled}"
+  alarm_name                = "${format(module.httpcode_alarm_label.id, "3XX")}"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "${var.evaluation_periods}"
+  metric_name               = "HTTPCode_Target_3XX_Count"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "${var.period}"
+  statistic                 = "Sum"
+  threshold                 = "${local.thresholds["target_3xx_count"]}"
+  treat_missing_data        = "${var.treat_missing_data}"
+  alarm_description         = "${format(var.httpcode_alarm_description, "3XX", module.default_label.id, local.thresholds["target_3xx_count"], var.period/60, var.evaluation_periods)}"
+  alarm_actions             = ["${local.alarm_actions}"]
+  ok_actions                = ["${local.ok_actions}"]
+  insufficient_data_actions = ["${local.insufficient_data_actions}"]
 
   dimensions = "${local.dimensions_map}"
 }
 
 resource "aws_cloudwatch_metric_alarm" "httpcode_target_4xx_count" {
-  count               = "${local.target_4xx_alarm_enabled}"
-  alarm_name          = "${format(module.httpcode_alarm_label.id, "4XX")}"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "${var.evaluation_periods}"
-  metric_name         = "HTTPCode_Target_4XX_Count"
-  namespace           = "AWS/ApplicationELB"
-  period              = "${var.period}"
-  statistic           = "Sum"
-  threshold           = "${local.thresholds["target_4xx_count"]}"
-  alarm_description   = "${format(var.httpcode_alarm_description, "4XX", module.default_label.id, local.thresholds["target_4xx_count"], var.period/60, var.evaluation_periods)}"
-  alarm_actions       = ["${var.notify_arns}"]
-  ok_actions          = ["${var.notify_arns}"]
+  count                     = "${local.target_4xx_alarm_enabled}"
+  alarm_name                = "${format(module.httpcode_alarm_label.id, "4XX")}"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "${var.evaluation_periods}"
+  metric_name               = "HTTPCode_Target_4XX_Count"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "${var.period}"
+  statistic                 = "Sum"
+  threshold                 = "${local.thresholds["target_4xx_count"]}"
+  treat_missing_data        = "${var.treat_missing_data}"
+  alarm_description         = "${format(var.httpcode_alarm_description, "4XX", module.default_label.id, local.thresholds["target_4xx_count"], var.period/60, var.evaluation_periods)}"
+  alarm_actions             = ["${local.alarm_actions}"]
+  ok_actions                = ["${local.ok_actions}"]
+  insufficient_data_actions = ["${local.insufficient_data_actions}"]
 
   dimensions = "${local.dimensions_map}"
 }
 
 resource "aws_cloudwatch_metric_alarm" "httpcode_target_5xx_count" {
-  count               = "${local.target_5xx_alarm_enabled}"
-  alarm_name          = "${format(module.httpcode_alarm_label.id, "5XX")}"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "${var.evaluation_periods}"
-  metric_name         = "HTTPCode_Target_5XX_Count"
-  namespace           = "AWS/ApplicationELB"
-  period              = "${var.period}"
-  statistic           = "Sum"
-  threshold           = "${local.thresholds["target_5xx_count"]}"
-  alarm_description   = "${format(var.httpcode_alarm_description, "5XX", module.default_label.id, local.thresholds["target_5xx_count"], var.period/60, var.evaluation_periods)}"
-  alarm_actions       = ["${var.notify_arns}"]
-  ok_actions          = ["${var.notify_arns}"]
+  count                     = "${local.target_5xx_alarm_enabled}"
+  alarm_name                = "${format(module.httpcode_alarm_label.id, "5XX")}"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "${var.evaluation_periods}"
+  metric_name               = "HTTPCode_Target_5XX_Count"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "${var.period}"
+  statistic                 = "Sum"
+  threshold                 = "${local.thresholds["target_5xx_count"]}"
+  treat_missing_data        = "${var.treat_missing_data}"
+  alarm_description         = "${format(var.httpcode_alarm_description, "5XX", module.default_label.id, local.thresholds["target_5xx_count"], var.period/60, var.evaluation_periods)}"
+  alarm_actions             = ["${local.alarm_actions}"]
+  ok_actions                = ["${local.ok_actions}"]
+  insufficient_data_actions = ["${local.insufficient_data_actions}"]
 
   dimensions = "${local.dimensions_map}"
 }
@@ -85,18 +96,20 @@ module "target_response_time_alarm_label" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "target_response_time_average" {
-  count               = "${local.target_response_time_alarm_enabled}"
-  alarm_name          = "${format(module.target_response_time_alarm_label.id)}"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "${var.evaluation_periods}"
-  metric_name         = "TargetResponseTime"
-  namespace           = "AWS/ApplicationELB"
-  period              = "${var.period}"
-  statistic           = "Average"
-  threshold           = "${local.thresholds["target_response_time"]}"
-  alarm_description   = "${format(var.target_response_time_alarm_description, module.default_label.id, local.thresholds["target_response_time"], var.period/60, var.evaluation_periods)}"
-  alarm_actions       = ["${var.notify_arns}"]
-  ok_actions          = ["${var.notify_arns}"]
+  count                     = "${local.target_response_time_alarm_enabled}"
+  alarm_name                = "${format(module.target_response_time_alarm_label.id)}"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "${var.evaluation_periods}"
+  metric_name               = "TargetResponseTime"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "${var.period}"
+  statistic                 = "Average"
+  threshold                 = "${local.thresholds["target_response_time"]}"
+  treat_missing_data        = "${var.treat_missing_data}"
+  alarm_description         = "${format(var.target_response_time_alarm_description, module.default_label.id, local.thresholds["target_response_time"], var.period/60, var.evaluation_periods)}"
+  alarm_actions             = ["${local.alarm_actions}"]
+  ok_actions                = ["${local.ok_actions}"]
+  insufficient_data_actions = ["${local.insufficient_data_actions}"]
 
   dimensions = "${local.dimensions_map}"
 }
