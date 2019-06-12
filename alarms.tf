f2c6af27e659ed7e0de7339e30da7e62a3ab6d11
Aaron Vinson <avinson.public@gmail.com>
2019-06-12 13:44:47 -0500

    check in updates

--- alarms.tf
+++ alarms.tf
@@ -1,22 +1,31 @@
 locals {
   # default to using notify_arns unless a more specific action is specified.
-  alarm_actions             = "${coalescelist(var.alarm_actions,var.notify_arns)}"
-  ok_actions                = "${coalescelist(var.ok_actions,var.notify_arns)}"
-  insufficient_data_actions = "${coalescelist(var.insufficient_data_actions,var.notify_arns)}"
+  alarm_actions                  = "${coalescelist(var.alarm_actions,var.notify_arns)}"
+  alarm_warn_actions             = "${coalescelist(var.alarm_warn_actions,var.notify_warn_arns)}"
+  ok_actions                     = "${coalescelist(var.ok_actions,var.notify_arns)}"
+  ok_warn_actions                = "${coalescelist(var.ok_warn_actions,var.notify_warn_arns)}"
+  insufficient_data_actions      = "${coalescelist(var.insufficient_data_actions,var.notify_arns)}"
+  insufficient_data_warn_actions = "${coalescelist(var.insufficient_data_warn_actions,var.notify_warn_arns)}"
 
   thresholds = {
-    elb_5xx_count             = "${max(var.elb_5xx_count_threshold, 0)}"
-    target_4xx_count          = "${max(var.target_4xx_count_threshold, 0)}"
-    target_5xx_count          = "${max(var.target_5xx_count_threshold, 0)}"
-    target_healthy_host_count = "${max(var.target_healthy_host_count_threshold, 0)}"
-    target_response_time      = "${max(var.target_response_time_threshold, 0)}"
+    elb_5xx_count                  = "${max(var.elb_5xx_count_threshold, 0)}"
+    elb_5xx_warn_count             = "${max(var.elb_5xx_warn_count_threshold, 0)}"
+    target_4xx_count               = "${max(var.target_4xx_count_threshold, 0)}"
+    target_4xx_warn_count          = "${max(var.target_4xx_warn_count_threshold, 0)}"
+    target_5xx_count               = "${max(var.target_5xx_count_threshold, 0)}"
+    target_5xx_warn_count          = "${max(var.target_5xx_warn_count_threshold, 0)}"
+    target_healthy_host_count      = "${max(var.target_healthy_host_count_threshold, 0)}"
+    target_healthy_host_warn_count = "${max(var.target_healthy_host_warn_count_threshold, 0)}"
+    target_response_time           = "${max(var.target_response_time_threshold, 0)}"
+    target_response_warn_time      = "${max(var.target_response_warn_time_threshold, 0)}"
   }
 
-  elb_5xx_alarm_enabled              = "${floor(var.elb_5xx_count_threshold)             < 0     ? 0 : 1 * local.enabled}"
-  target_4xx_alarm_enabled           = "${floor(var.target_4xx_count_threshold)          < 0     ? 0 : 1 * local.enabled}"
-  target_5xx_alarm_enabled           = "${floor(var.target_5xx_count_threshold)          < 0     ? 0 : 1 * local.enabled}"
-  target_healthy_host_alarm_enabled  = "${floor(var.target_healthy_host_count_threshold) < 0     ? 0 : 1 * local.enabled}"
-  target_response_time_alarm_enabled = "${floor(var.target_response_time_threshold)      < 0 ? 0 : 1 * local.enabled}"
+  elb_5xx_alarm_enabled              = "${floor(var.elb_5xx_count_threshold)                  <= 0     ? 0 : 1 * local.enabled}"
+  target_4xx_alarm_enabled           = "${floor(var.target_4xx_count_threshold)               <= 0     ? 0 : 1 * local.enabled}"
+  target_5xx_alarm_enabled           = "${floor(var.target_5xx_count_threshold)               <= 0     ? 0 : 1 * local.enabled}"
+  target_healthy_host_alarm_enabled  = "${floor(var.target_healthy_host_count_threshold)      <= 0     ? 0 : 1 * local.enabled}"
+  target_healthy_host_alarm_enabled  = "${floor(var.target_healthy_host_warn_count_threshold) <= 0     ? 0 : 1 * local.enabled}"
+  target_response_time_alarm_enabled = "${floor(var.target_response_time_threshold)           <= 0 ? 0 : 1 * local.enabled}"
 
   target_group_dimensions_map = {
     "TargetGroup"  = "${var.target_group_arn_suffix}"
@@ -55,6 +64,25 @@ resource "aws_cloudwatch_metric_alarm" "httpcode_target_4xx_count" {
   dimensions = "${local.target_group_dimensions_map}"
 }
 
+resource "aws_cloudwatch_metric_alarm" "httpcode_target_4xx_warn_count" {
+  count                     = "${local.target_4xx_alarm_warn_enabled}"
+  alarm_name                = "${format(module.httpcode_alarm_label.id, "4XX")}"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "${var.evaluation_periods}"
+  metric_name               = "HTTPCode_Target_4XX_Count"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "${var.period}"
+  statistic                 = "Sum"
+  threshold                 = "${local.thresholds["target_4xx_warn_count"]}"
+  treat_missing_data        = "${var.treat_missing_data}"
+  alarm_description         = "${format(var.httpcode_alarm_description, "4XX", module.default_label.id, local.thresholds["target_4xx_count_warn"], var.period/60, var.evaluation_periods)}"
+  alarm_actions             = ["${local.alarm_warn_actions}"]
+  ok_actions                = ["${local.ok_warn_actions}"]
+  insufficient_data_actions = ["${local.insufficient_data_warn_actions}"]
+
+  dimensions = "${local.target_group_dimensions_map}"
+}
+
 resource "aws_cloudwatch_metric_alarm" "httpcode_target_5xx_count" {
   count                     = "${local.target_5xx_alarm_enabled}"
   alarm_name                = "${format(module.httpcode_alarm_label.id, "5XX")}"
@@ -74,6 +102,25 @@ resource "aws_cloudwatch_metric_alarm" "httpcode_target_5xx_count" {
   dimensions = "${local.target_group_dimensions_map}"
 }
 
+resource "aws_cloudwatch_metric_alarm" "httpcode_target_5xx_warn_count" {
+  count                     = "${local.target_5xx_alarm_warn_enabled}"
+  alarm_name                = "${format(module.httpcode_alarm_label.id, "5XX")}"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = "${var.evaluation_periods}"
+  metric_name               = "HTTPCode_Target_5XX_Count"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "${var.period}"
+  statistic                 = "Sum"
+  threshold                 = "${local.thresholds["target_5xx_warn_count"]}"
+  treat_missing_data        = "${var.treat_missing_data}"
+  alarm_description         = "${format(var.httpcode_alarm_description, "5XX", module.default_label.id, local.thresholds["target_5xx_warn_count"], var.period/60, var.evaluation_periods)}"
+  alarm_actions             = ["${local.alarm_warn_actions}"]
+  ok_actions                = ["${local.ok_warn_actions}"]
+  insufficient_data_actions = ["${local.insufficient_data_warn_actions}"]
+
+  dimensions = "${local.target_group_dimensions_map}"
+}
+
 resource "aws_cloudwatch_metric_alarm" "healthy_host_target_count" {
   count                     = "${local.target_healthy_host_alarm_enabled}"
   alarm_name                = "${format(module.httpcode_alarm_label.id, "5XX")}"
@@ -85,10 +132,29 @@ resource "aws_cloudwatch_metric_alarm" "healthy_host_target_count" {
   statistic                 = "Minimum"
   threshold                 = "${local.thresholds["target_healthy_host_count"]}"
   treat_missing_data        = "${var.treat_missing_data}"
-  alarm_description         = "${format(var.httpcode_alarm_description, "5XX", module.default_label.id, local.thresholds["target_5xx_count"], var.period/60, var.evaluation_periods)}"
+  alarm_description         = "${format(var.target_healthy_host_alarm_description, module.default_label.id, local.thresholds["target_healthy_host_count"], var.period/60)}"
   alarm_actions             = ["${local.alarm_actions}"]
   ok_actions                = ["${local.ok_actions}"]
   insufficient_data_actions = ["${local.insufficient_data_actions}"]
+
+  dimensions = "${local.target_group_dimensions_map}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "healthy_host_target_warn_count" {
+  count                     = "${local.target_healthy_host_alarm_warn_enabled}"
+  alarm_name                = "${format(module.httpcode_alarm_label.id, "5XX")}"
+  comparison_operator       = "LessThanThreshold"
+  evaluation_periods        = "${var.evaluation_periods}"
+  metric_name               = "HealthyHostCount"
+  namespace                 = "AWS/ApplicationELB"
+  period                    = "${var.period}"
+  statistic                 = "Minimum"
+  threshold                 = "${local.thresholds["target_healthy_host_warn_count"]}"
+  treat_missing_data        = "${var.treat_missing_data}"
+  alarm_description         = "${format(var.target_healthy_host_alarm_description, module.default_label.id, local.thresholds["target_healthy_host_warn_count"], var.period/60)}"
+  alarm_actions             = ["${local.alarm_warn_actions}"]
+  ok_actions                = ["${local.ok_warn_actions}"]
+  insufficient_data_actions = ["${local.insufficient_data_warn_actions}"]
 
   dimensions = "${local.target_group_dimensions_map}"
 }
--- variables.tf
+++ variables.tf
@@ -5,7 +5,8 @@ variable "name" {
 
 variable "namespace" {
   type        = "string"
-  description = "Namespace (e.g. `cp` or `cloudposse`)"
+  description = "Namespace (e.g. `lyr` or `lyric`)"
+  default     = "lyric"
 }
 
 variable "delimiter" {
@@ -43,9 +44,21 @@ variable "notify_arns" {
   default     = []
 }
 
+variable "notify_warn_arns" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to execute when this WARNING alarm transitions into ANY state from any other state. May be overridden by the value of a more specific {alarm,ok,insufficient_data}_warn_actions variable. "
+  default     = []
+}
+
 variable "alarm_actions" {
   type        = "list"
   description = "A list of ARNs (i.e. SNS Topic ARN) to execute when this alarm transitions into an ALARM state from any other state.  If set, this list takes precedence over notify_arns."
+  default     = []
+}
+
+variable "alarm_warn_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to execute when this WARNING alarm transitions into an ALARM state from any other state.  If set, this list takes precedence over notify_warn_arns."
   default     = []
 }
 
@@ -55,9 +68,21 @@ variable "ok_actions" {
   default     = []
 }
 
+variable "ok_warn_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to execute when this WARNING alarm transitions into an OK state from any other state. If set, this list takes precedence over notify_warn_arns."
+  default     = []
+}
+
 variable "insufficient_data_actions" {
   type        = "list"
   description = "A list of ARNs (i.e. SNS Topic ARN) to execute when this alarm transitions into an INSUFFICIENT_DATA state from any other state. If set, this list takes precedence over notify_arns."
+  default     = []
+}
+
+variable "insufficient_data_warn_actions" {
+  type        = "list"
+  description = "A list of ARNs (i.e. SNS Topic ARN) to execute when this WARNING alarm transitions into an INSUFFICIENT_DATA state from any other state. If set, this list takes precedence over notify_warn_arns."
   default     = []
 }
 
@@ -84,31 +109,49 @@ variable "target_group_arn_suffix" {
 variable "evaluation_periods" {
   type        = "string"
   description = "Number of periods to evaluate for the alarm."
-  default     = "1"
+  default     = "2"
 }
 
 variable "period" {
   type        = "string"
   description = "Duration in seconds to evaluate for the alarm."
-  default     = "300"
+  default     = "60"
 }
 
 variable "target_4xx_count_threshold" {
   type        = "string"
-  description = "The maximum count of 4XX requests over a period. A negative value will disable the alert."
+  description = "The maximum count of 4XX requests over a period. A value `<=0` will disable the alert."
   default     = "25"
+}
+
+variable "target_4xx_warn_count_threshold" {
+  type        = "string"
+  description = "The maximum count of 4XX requests over a period. A value `<=0` will disable the alert."
+  default     = "0"
 }
 
 variable "target_5xx_count_threshold" {
   type        = "string"
-  description = "The maximum count of 5XX requests over a period. A negative value will disable the alert."
+  description = "The maximum count of 5XX requests over a period. A value `<=0` will disable the alert."
   default     = "25"
+}
+
+variable "target_5xx_warn_count_threshold" {
+  type        = "string"
+  description = "The maximum count of 5XX requests over a period. A value `<=0` will disable the alert."
+  default     = "0"
 }
 
 variable "elb_5xx_count_threshold" {
   type        = "string"
-  description = "The maximum count of ELB 5XX requests over a period. A negative value will disable the alert."
+  description = "The maximum count of ELB 5XX requests over a period. A value `<=0` will disable the alert."
   default     = "25"
+}
+
+variable "elb_5xx_warn_count_threshold" {
+  type        = "string"
+  description = "The maximum count of ELB 5XX requests over a period. A value `<=0` will disable the alert."
+  default     = "0"
 }
 
 variable "httpcode_alarm_description" {
@@ -117,10 +160,34 @@ variable "httpcode_alarm_description" {
   default     = "HTTPCode %v count for %v over %v last %d minute(s) over %v period(s)"
 }
 
+variable "target_healthy_host_count_threshold" {
+  type        = "string"
+  description = "A value below which an alert will trigger for healthy hosts in a target group. A value `<=0` will disable the alert."
+  default     = "1"
+}
+
+variable "target_healthy_host_warn_count_threshold" {
+  type        = "string"
+  description = "A value below which a WARNING alert will trigger for healthy hosts in a target group. A value `<=0` will disable the alert."
+  default     = "0"
+}
+
+variable "target_healthy_host_alarm_description" {
+  type        = "string"
+  description = "The string to format and use as the healthy host alarm description."
+  default     = "Healthy hosts below %v for %v over %v last %d minute(s)"
+}
+
 variable "target_response_time_threshold" {
   type        = "string"
-  description = "The maximum average target response time (in seconds) over a period. A negative value will disable the alert."
-  default     = "0.5"
+  description = "The maximum average target response time (in seconds) over a period. A value `<=0` will disable the alert."
+  default     = "0"
+}
+
+variable "target_response_time_warn_threshold" {
+  type        = "string"
+  description = "The maximum average target response time (in seconds) over a period. A value `<=0` will disable the alert."
+  default     = "0"
 }
 
 variable "target_response_time_alarm_description" {
