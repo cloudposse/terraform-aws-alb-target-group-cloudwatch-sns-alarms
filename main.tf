3e6b22e2e884201ee33e2b5075149c4969a8959c
Aaron Vinson <avinson.public@gmail.com>
2019-06-14 12:19:03 -0700

    use fork of label module for default_label

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "default" {}
 
 module "default_label" {
-  source    = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
+  source    = "git::https://github.com/pricemethod/terraform-terraform-label.git?ref=master"
   name      = "${var.name}"
   namespace = "${var.namespace}"
   stack     = "${var.stack}"
