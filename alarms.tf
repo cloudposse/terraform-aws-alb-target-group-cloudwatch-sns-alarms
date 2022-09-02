de2416df25805b7423880cee670c88b211bb4077
Johannes Ziemke <github@5pi.de>
2022-09-02 17:22:30 +0200

    Use terraform-null-label instead of terraform-terraform-label

--- alarms.tf
+++ alarms.tf
@@ -49,7 +49,8 @@ locals {
 }
 
 module "httpcode_alarm_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.5.1"
+  source     = "cloudposse/label/null"
+  version    = "v0.25.0"
   name       = var.name
   namespace  = var.namespace
   stage      = var.stage
@@ -57,7 +58,8 @@ module "httpcode_alarm_label" {
 }
 
 module "httpcode_rate_alarm_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.5.1"
+  source     = "cloudposse/label/null"
+  version    = "v0.25.0"
   name       = var.name
   namespace  = var.namespace
   stage      = var.stage
@@ -463,7 +465,8 @@ resource "aws_cloudwatch_metric_alarm" "httpcode_elb_5xx_count" {
 }
 
 module "target_response_time_alarm_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.5.1"
+  source     = "cloudposse/label/null"
+  version    = "v0.25.0"
   name       = var.name
   namespace  = var.namespace
   stage      = var.stage
--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ data "aws_caller_identity" "default" {
 }
 
 module "default_label" {
-  source    = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.5.1"
+  source    = "cloudposse/label/null"
+  version   = "v0.25.0"
   name      = var.name
   namespace = var.namespace
   stage     = var.stage
